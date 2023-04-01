3ca0d54bbfa942477c394ba77b204896caaab20e
a.y.volkov <a.y.volkov@yadro.com>
2023-04-01 23:39:41 +0300

    Initial commit.

---
+++ postgres/3_sql_enjection.py
@@ -0,0 +1,33 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from helper.helper import TABLE_NAME, PgDbConfig
+
+connection = psycopg2.connect(**PgDbConfig().asdict())
+cursor = connection.cursor()
+
+connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+insert_stmt = f'INSERT INTO {TABLE_NAME} (name, age) VALUES (%s, %s);'
+cursor.execute(insert_stmt, ('Alice', 20))
+cursor.execute(insert_stmt, ('Bob', 30))
+
+# 1' or 1=1;--
+# 1';delete from persons;--
+# 1';drop table persons;--
+while True:
+    some_name = input('insert name: ')
+    if 'exit' in some_name:
+        break
+
+    sql_stmt = f"SELECT * FROM {TABLE_NAME} WHERE name='{some_name}'"
+    try:
+        cursor.execute(sql_stmt)
+        results = cursor.fetchall()
+
+        for row in results:
+            print(row)
+    except Exception as err:
+        print(err)
+
+cursor.close()
+connection.close()
